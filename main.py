6c12550969861b4052370546db5f9629290a9d66
Alex Kosh <sasha23969@gmail.com>
2023-02-22 17:52:46 +0300

    docs: Add todo about possible ord errors

--- main.py
+++ main.py
@@ -107,6 +107,7 @@ async def order(
         order_json = json.dumps(order_dict, ensure_ascii=False, sort_keys=True, indent=2)
         r = tg_send_message(f'<b>New order</b>\n\n<code>{order_json}</code>', TG_ALERTS_CHANNEL)
 
+        # TODO: Add try/except
         ord_wrapper = OrdWrapper()
         prices = ord_wrapper.estimate_price(file.size, FEE_RATE)
         if decimal.Decimal(value_wei_decimal) < prices['total_price_wei']:
